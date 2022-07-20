bbe0870802e4d743a2867f696fcdf7f2e095004d
Ishtiyaq Husain <ishtiyaq.husain@gmail.com>
2022-07-20 18:56:09 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,14 +1,5 @@
-module "s3_bucket" {
-  #source = "terraform-aws-modules/s3-bucket/aws"
-  source = "ishtiyaq/terraform-aws-s3-bucket"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/tfdemo-2022/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = var.prefix
-
 }
